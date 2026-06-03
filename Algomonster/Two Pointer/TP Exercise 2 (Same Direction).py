4b9e8b9851b1644258cf6b3ffc91d78a34d68e5b
Wooseok Lee <wooseokprivate123@gmail.com>
2026-06-03 14:45:40 +0000

    Remove Cpp solutions for TP exercises

--- Algomonster/Two Pointer/TP Exercise 2 (Same Direction).py
+++ Algomonster/Two Pointer/TP Exercise 2 (Same Direction).py
@@ -1,15 +1,7 @@
 # LeetCode 876. Middle of the Linked List
 # https://leetcode.com/problems/middle-of-the-linked-list/description/
 
-int middle_of_linked_list(Node<int>* head) {
-    Node<int>* slow = head;
-    Node<int>* fast = head;
-    while (fast && fast->next) {
-        fast = fast->next->next;
-        slow = slow->next;
-    }
-    return slow->val;
-}
+maincode
 
 """
 Notes
